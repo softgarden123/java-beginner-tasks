d2db9763bceccd193fffa08217e3d19bd1931154
SY <ogin1234@gmail.com>
2025-10-20 22:20:26 +0900

    03_task パッケージ変更

---
+++ 03_age_handler/src/AgeHandler.java
@@ -0,0 +1,15 @@
+public class AgeHandler {
+    private static final String CHILD = "あなたは子どもです。";
+    private static final String TEENAGER = "あなたはティーンエイジャーです。";
+    private static final String ADULT = "あなたは大人です。";
+    private static final String SENIOR = "あなたはシニアです。";
+
+    public static String getMessage(int age) {
+        // 年齢に応じたメッセージを返却してください。
+        // 13歳未満: CHILD
+        // 13歳以上20歳未満: TEENAGER
+        // 20歳以上65歳未満: ADULT
+        // 65歳以上: SENIOR
+    }
+
+}
